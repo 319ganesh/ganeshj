80fca014a4578361bdb40d043b77a646ab0d0a53
319ganesh <31438442+319ganesh@users.noreply.github.com>
2018-02-28 02:38:01 -0800

    Create space.c

---
+++ space.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+main()
+{
+char s[100];
+int i,b=0;
+printf("\n enter the sentence:");
+gets(s);
+for(i=0;i<=strlen(s);i++)
+{
+if(s[i]==' ')
+b++;
+}
+printf("\n the value is:%d",b);
+return 0;
+}
