146a80233b177a3c2abe5c8bc363a3e4dd1aeba7
319ganesh <31438442+319ganesh@users.noreply.github.com>
2018-03-07 03:16:11 -0800

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("\n the range of a should be between 1 to 10");
+	printf("\n enter the value of a:");
+	scanf("%d",&a);
+	if(a==1)
+	{
+		printf("\n one",a);
+	}
+	else if(a==2)
+	{
+		printf("\n two",a);
+	}
+	else if(a==3)
+	{
+		printf("\n three",a);
+	}
+	else if(a==4)
+	{
+		printf("\n four",a);
+	}
+	else if(a==5)
+	{
+		printf("\n five",a);
+	}
+	else if(a==6)
+	{
+		printf("\n six",a);
+	}
+	else if(a==7)
+	{
+		printf("\n seven",a);
+	}
+	else if(a==8)
+	{
+		printf("\n eight",a);
+	}
+	else if(a==9)
+	{
+		printf("\n nine",a);
+	}
+	else if(a==10)
+	{
+		printf("\n ten",a);
+	}
+	else
+	{
+		printf("\n the value is not in range");
+	}
+	return 0;
+}
