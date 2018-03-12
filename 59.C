745ad07b07f0627f4d484d3a0d546104a4b53882
319ganesh <31438442+319ganesh@users.noreply.github.com>
2018-03-12 16:29:26 +0530

    Create 59.C

---
+++ 59.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+int a[10],i,n,temp;
+printf("\n enter the value of n");
+scanf("%d",&n);
+printf("\n enter the value:");
+for(i=0;i<=n;i++)
+	{
+          scanf("%d",&a[i]);
+	}
+	for(i=0;i<=n;i++)
+	{
+		if(a[i]>a[i+1])
+		{
+		temp=a[i];
+		a[i]=a[i+1];
+		a[i+1]=temp;
+		}
+	}
+	printf("\n the higest value is:%d",a[i]);
+	return 0;
+}
